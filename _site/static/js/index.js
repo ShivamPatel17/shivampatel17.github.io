ceac73d29f5a7ef58e8667354b8c54a2895620d1
ShivamPatel17 <patel.shivam.m@gmail.com>
2019-10-12 01:48:45 -0400

    got rid of .row1 from workexperience.html

--- _site/static/js/index.js
+++ _site/static/js/index.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
 
     $("#workexperience").click(function(){
-        window.location.replace("/workexperience")
+        window.location.replace("/workexperience");
+    })
+    $("#aboutme").click(function(){
+        alert("This page is currently being created!");
     })
   
 });
