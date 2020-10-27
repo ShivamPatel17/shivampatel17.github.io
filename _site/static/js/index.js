0e14c90c52e2fe9ae27bfd27aa8f35e9bb07429f
Shivam <patel.shivam.m@gmail.com>
2020-10-27 19:12:13 -0400

    made everything dropdown

--- _site/static/js/index.js
+++ _site/static/js/index.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
       
       
 });
-var up = false;
+var up0 = false;
 function toggleExperiences() {
     document.getElementById("education").classList.toggle("show");
     document.getElementById("career").classList.toggle("show");
@@ -18,13 +18,43 @@ function toggleExperiences() {
 
     var arrows = document.getElementsByClassName("arrow-img");
     var size = arrows.length;
-    for(var i = 0; i<size; i++){
-        var arrow = arrows[i];
-        if(!up){
-            arrow.style.transform = "scaleY(-1)";
-        }else{
-            arrow.style.transform = "scaleY(1)";
-        }
+    var arrow = arrows[0];
+    if(!up0){
+        arrow.style.transform = "scaleY(-1)";
+    }else{
+        arrow.style.transform = "scaleY(1)";
     }
-    up = !up;
+    up0 = !up0;
+}
+
+
+var up1 = false;
+function toggleProjects() {
+    document.getElementById("projects").classList.toggle("show");
+
+
+    var arrows = document.getElementsByClassName("arrow-img");
+    var size = arrows.length;
+    var arrow = arrows[1];
+    if(!up1){
+        arrow.style.transform = "scaleY(-1)";
+    }else{
+        arrow.style.transform = "scaleY(1)";
+    }
+    up1 = !up1;
+}
+var up2 = false;
+function togglePodcasts() {
+    document.getElementById("podcasts-dd").classList.toggle("show");
+
+
+    var arrows = document.getElementsByClassName("arrow-img");
+    var size = arrows.length;
+    var arrow = arrows[2];
+    if(!up2){
+        arrow.style.transform = "scaleY(-1)";
+    }else{
+        arrow.style.transform = "scaleY(1)";
+    }
+    up2 = !up2;
 }
